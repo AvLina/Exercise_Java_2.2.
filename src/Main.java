0c242cd5fef3d4e857e8737de18905edd3d98c78
Lina Avramenko <avlina3@gmail.com>
2021-11-07 18:48:47 +0300

    weight indicator

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        float weight = 55;
-        double height = 1.68;
+        float weight = 57;
+        double height = 1.58;
         double bmi = service.calculate(weight, height);
-//        double bmi = weight / (height * height);
+        if ((bmi > 18)&& (bmi < 25)){
+          // параметр "18" должен быть "18,5", но я не знаю как записать чтобы идея не ругалась
+            System.out.println("NORMA");
+        }
+        if (bmi > 25) {
+            System.out.println("OVERWEIGHT");
+        }
+        if (bmi < 18) {
+            System.out.println("UNDERWEIGH");
+        }
         System.out.println(bmi);
+
     }
+
 }
